a3fd492b4df4ab5441132aabc48695433cc367ef
Benjamin KAPOOR <benjaminkapoor@yahoo.fr>
2019-10-10 12:14:34 +0200

    commit v2 medecin

--- MedecinPatient/src/eu/ensup/master2/medecinpatient/domaine/Medecin.java
+++ MedecinPatient/src/eu/ensup/master2/medecinpatient/domaine/Medecin.java
@@ -1,7 +1,6 @@
 package eu.ensup.master2.medecinpatient.domaine;
 
 /**
-<<<<<<< HEAD
  * @author benja
  *
  *         Class metier de Patient associé à celle de Medecin
@@ -50,75 +49,4 @@ public class Medecin extends Personne {
 	public String toString() {
 		return "Medecin [disponibilite=" + this.disponibilite + ", patient=" + this.patient + "]";
 	}
-
-=======
- * @author benja Class metier de Patient associé à celle de Medecin
- */
-public class Medecin {
-
-	private int id;
-	private String nom;
-	private String prenom;
-	private String adresse;
-	private Patient patient;
-
-	public Medecin() {
-		super();
-	}
-
-	public Medecin(int id, String nom, String prenom, String adresse, Patient patient) {
-		super();
-		this.id = id;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.adresse = adresse;
-		this.patient = patient;
-	}
-
-	public String getAdresse() {
-		return this.adresse;
-	}
-
-	public int getId() {
-		return this.id;
-	}
-
-	public String getNom() {
-		return this.nom;
-	}
-
-	public Patient getPatient() {
-		return this.patient;
-	}
-
-	public String getPrenom() {
-		return this.prenom;
-	}
-
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public void setPatient(Patient patient) {
-		this.patient = patient;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	@Override
-	public String toString() {
-		return "Medecin [id=" + this.id + ", nom=" + this.nom + ", prenom=" + this.prenom + ", adresse=" + this.adresse
-				+ ", patient=" + this.patient + "]";
-	}
->>>>>>> branch 'master' of https://github.com/BenKapoor/Medecin
 }
